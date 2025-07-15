34c262712b68df14604c59b4611ce5c3ba463fa2
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2025-07-14 22:56:34 -0300

    adiciona knapsack execução iterativa

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,45 @@ import time
 app = Flask(__name__)
 CORS(app)
 
+def solve_knapsack_iterative(items, capacity):
+    """
+    Resolve o problema da Mochila (knapsack) 0/1 usando a abordagem de Programação Dinâmica bottom-up (iterativa).
+    """
+    start_time = time.perf_counter()
+    num_items = len(items)
+    dp_table = [[0 for _ in range(capacity + 1)] for _ in range(num_items + 1)]
+    
+    for i in range(1, num_items + 1):
+        name, weight, value, emoji = items[i-1].values()
+        
+        for w in range(1, capacity + 1):
+            if weight > w:
+                dp_table[i][w] = dp_table[i-1][w]
+            else:
+                value_if_included = value + dp_table[i-1][w - weight]
+                value_if_excluded = dp_table[i-1][w]
+                dp_table[i][w] = max(value_if_included, value_if_excluded)
+
+    max_value = dp_table[num_items][capacity]
+    
+    selected_items = []
+    w = capacity
+    for i in range(num_items, 0, -1):
+        if dp_table[i][w] != dp_table[i-1][w]:
+            item = items[i-1]
+            selected_items.append(item)
+            w -= item['weight']
+    
+    selected_items.reverse()
+    end_time = time.perf_counter()
+
+    return {
+        "selected_items": selected_items,
+        "total_value": max_value,
+        "dp_table": dp_table,
+        "execution_time": (end_time - start_time) * 1000  # in milliseconds
+    }
+
 @app.route('/api/knapsack', methods=['GET'])
 def knapsack_endpoint():
     items = [
@@ -29,9 +68,15 @@ def knapsack_endpoint():
         # desenvolver recursivo
         
     else: # Default to iterative
-        # desenvolver iterativo
+        result = solve_knapsack_iterative(items, capacity)
     
-    response = True
+    response = {
+        "items": items,
+        "capacity": capacity,
+        "result": result,
+        "mode": mode,
+        "instant": instant
+    }
     
     return jsonify(response)
 
